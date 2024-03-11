17a11f788415c13c505add1bb6dcfd69becfd09a
Thomas-Koit <159206629+Thomas-Koit@users.noreply.github.com>
2024-03-11 14:00:57 +1100

    Update

---
+++ script.js
@@ -0,0 +1,20 @@
+let enterName = prompt("Enter your name");
+let playerGuess
+let playerFeedback = "TEST"
+let computerGuess
+loadDIV ();
+
+
+
+function loadDIV () {
+    document.getElementById("ScriptThisDIV").innerHTML = "<H1>Hello " + enterName + "</H1>"
+    document.getElementById("ScriptThisDIV").innerHTML = document.getElementById("ScriptThisDIV").innerHTML + "<H2>Guess a number between 1 & 10"
+    document.getElementById("ScriptThisDIV").innerHTML = document.getElementById("ScriptThisDIV").innerHTML + playerFeedback
+}
+
+function enterNumber () {
+    playerGuess = prompt ("Enter a number")
+}
+
+
+
