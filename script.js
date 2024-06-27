27de0959fe175eeb32cd6a7c94aa305882f32601
Thomas-Koit <159206629+Thomas-Koit@users.noreply.github.com>
2024-06-27 10:35:19 +1000

    Javascript

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function darkMode() {
-   var element = document.body;
+   var element = document.getElementById("NavBarDiv");
 
    element.classList.toggle("dark-mode");
+}
+
+function darkMode2() {
+   var element = document.body;
+
+   element.classList.toggle("darkmode")
 }
